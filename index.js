ceeb8844fe8f19cd9b906b888f046d2819d1bc9e
Amanda Nunes <carvalho_amanda@hotmail.com>
2021-05-27 22:05:36 -0300

    arquivo index atualizado

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
-
 app.use(express.json());
-// serve para aceitar a comunicação em json
 
 const port = 3000;
 
-const usuarioAmanda = {
-    id: 1,
-    nome: 'Amanda',
+const usuarioFernando = {
+    id: 0,
+    nome: 'Amanda Nunes',
     idade: 32
 };
 
-const usuarios = [usuarioAmanda];
+const usuarios = [usuarioFernando];
 
-app.get('/usuarios', (req,res) => {
+app.get('/usuarios', (req, res) => {
     res.json(usuarios);
 });
 
+app.post('/usuarios', (req, res) => {
+    const { body: { nome, idade } } = req;
+    const id = usuarios.length;
+
+    const novoUsuario = {
+        id,
+        nome,
+        idade
+    };
+
+    usuarios.push(novoUsuario);
+    res.status(201).json(novoUsuario);
+});
+
+app.put('/usuarios/:id', (req, res) => {
+    const { id } = req.params;
+    const { nome, idade } = req.body;
+
+    const usuario = usuarios.find(usuario => usuario.id === Number(id));
+    usuario.nome = nome;
+    usuario.idade = idade;
+    
+
+    res.json(usuario);
+});
+
+app.delete('/usuarios/:id', (req, res) => {
+    const { id } = req.params;
+
+    const index = usuarios.findIndex(usuario => usuario.id === Number(id));
+    
+    usuarios.splice(index, 1);
+
+    res.status(204).send();
+});
+
 app.listen(port, () => {
-    console.log(`Server is up and running on port ${port}`)
+    console.log(`Server is up and running on port ${port}`);
 });
